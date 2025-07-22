b7917e6be7d41c930bb42d8cee14c2daeeb9c0c6
Fernando Rogelin <fernando.rogelin@hotmail.com>
2025-07-22 11:44:24 -0300

    feat: Create validation on server.js

--- server/server.js
+++ server/server.js
@@ -15,11 +15,28 @@ app.use('/registration', express.static(fileURLToPath(new URL('../web/dist', imp
 app.get('', (req, res) => res.redirect('/registration'))
 
 app.post('/registration', (req, res) => {
-  const { email, password } = req.body
+  const { personType, email, password } = req.body
 
-  if (!email) return res.status(400).json({ error: 'E-mail obrigatório!' })
+  if (!email) return res.status(422).json({ error: 'E-mail obrigatório!' })
+  if (!password) return res.status(422).json({ error: 'Senha obrigatória!' })
 
-  if (!password) return res.status(400).json({ error: 'Senha obrigatória!' })
+  if (personType === 'person') {
+    const { name, cpf, birthdate, phone } = req.body
+
+    if (!name) return res.status(422).json({ error: 'Nome obrigatório!' })
+    if (!cpf) return res.status(422).json({ error: 'CPF obrigatório!' })
+    if (!birthdate) return res.status(422).json({ error: 'Data de nascimento obrigatória!' })
+    if (!phone) return res.status(422).json({ error: 'Telefone obrigatório!' })
+  }
+
+  if (personType === 'company') {
+    const { companyName, cnpj, openingDate, companyPhone } = req.body
+
+    if (!companyName) return res.status(422).json({ error: 'Razão social obrigatório!' })
+    if (!cnpj) return res.status(422).json({ error: 'CNPJ obrigatório!' })
+    if (!openingDate) return res.status(422).json({ error: 'Data de abertura obrigatório!' })
+    if (!companyPhone) return res.status(422).json({ error: 'Telefone obrigatório!' })
+  }
 
   res.status(200).json({ message: 'Cadastro realizado com sucesso!' })
 })
