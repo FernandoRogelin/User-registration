0c9995a0c00b4e3b98d8b961f074f0a3f1d1111f
Fernando Rogelin <fernando.rogelin@hotmail.com>
2025-07-25 21:34:29 -0300

    feat: Create a header tests

---
+++ web/src/components/__tests__/Header.spec.js
@@ -0,0 +1,46 @@
+import { describe, expect, test, afterEach } from "vitest"
+import { render, screen, cleanup } from "@testing-library/vue"
+
+import AppHeader from "../common/AppHeader.vue"
+
+import { FIRST_STEP, LAST_STEP, SECOND_STEP, THIRD_STEP } from "@/constants/steps"
+
+afterEach(() => cleanup())
+
+describe('<AppHeader /> common component', () => {
+  test('should render the component and match snapshot', () => {
+    const { container } = render(AppHeader)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('should render the first step', () => {
+    render(AppHeader, { props: { step: FIRST_STEP } })
+
+    expect(screen.getByRole('heading', { name: /Seja bem-vindo\(a\)/i }))
+  })
+
+  test('should render the second step private individuals', () => {
+    render(AppHeader, { props: { step: SECOND_STEP, isCompany: false } })
+
+    expect(screen.getByRole('heading', { name: 'Pessoa Física' }))
+  })
+
+  test('should render the second step legal person', () => {
+    render(AppHeader, { props: { step: SECOND_STEP, isCompany: true } })
+
+    expect(screen.getByRole('heading', { name: 'Pessoa Jurídica' }))
+  })
+
+  test('should render the third step', () => {
+    render(AppHeader, { props: { step: THIRD_STEP } })
+
+    expect(screen.getByRole('heading', { name: /Senha de acesso/i }))
+  })
+
+  test('should render the last step', () => {
+    render(AppHeader, { props: { step: LAST_STEP } })
+
+    expect(screen.getByRole('heading', { name: /Revise suas informações/i }))
+  })
+})
